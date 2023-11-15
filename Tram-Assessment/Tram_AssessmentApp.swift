99549d8ae9ef568a9214546f53cc38f946610b81
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-11-15 10:18:51 -0500

    Initial Commit

---
+++ Tram-Assessment/Tram_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tram_AssessmentApp.swift
+//  Tram-Assessment
+//
+//  Created by Gaurav Bhambhani on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Tram_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
