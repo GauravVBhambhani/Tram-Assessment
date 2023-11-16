1ca340ab18a17524083efab278264cf08e72b600
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-11-15 20:40:41 -0500

    Integrated Update User Details. So Admin can update any or all text feilds and even the image.

---
+++ Tram-Assessment/View/UpdateUserView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateUserView.swift
+//  Tram-Assessment
+//
+//  Created by Gaurav Bhambhani on 11/15/23.
+//
+
+import SwiftUI
+
+struct UpdateUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateUserView()
+}
---
+++ Tram-Assessment/ViewModel/UpdateUserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UpdateUserViewModel.swift
+//  Tram-Assessment
+//
+//  Created by Gaurav Bhambhani on 11/15/23.
+//
+
+import Foundation
